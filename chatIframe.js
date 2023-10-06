d8476753d03f2dcffbc5842a3fe08efbc1c3f1b6
VuIt96 <50444317+VuIt96@users.noreply.github.com>
2023-10-06 10:57:04 +0700

    Update chatIframe.js

--- chatIframe.js
+++ chatIframe.js
@@ -1,203 +1,90 @@
-async function init(t, e, l = "450px", s = "550px", iconLogo) {
-    const keyframesStyle = document.createElement("style");
-    keyframesStyle.textContent = `
-  @keyframes slideInFromRight {
-    0% {
-      transform: translateX(100%);
-    }
-    100% {
-      transform: translateX(0);
-    }
-  }
-`;
+async function init(t, e, l = "450px", s = "550px") {
+  let id = await t;
 
-    document.head.appendChild(keyframesStyle);
+  let i = `http://localhost:3001`,
+    o =
+      '<svg width="40" height="" viewBox="0 0 152 103" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M152 54.847V66.162C152 69.9021 148.978 72.9203 145.263 72.9203H144.287C143.111 89.301 129.543 102.249 112.977 102.249H108.686V87.3126C119.773 86.1645 128.415 76.7314 128.415 65.2743V57.4154C128.415 45.1772 118.562 35.2588 106.405 35.2588H45.1838C33.0149 35.2588 23.1504 45.1772 23.1504 57.4154V65.2743C23.1504 77.5126 33.0031 87.4309 45.1838 87.4309H93.9889L108.697 102.249H39.0229C22.4332 102.249 8.88861 89.301 7.71287 72.9203H6.73701C3.02166 72.9203 0 69.9021 0 66.162V54.847C0 51.1069 3.02166 48.0651 6.73701 48.0651H7.87748C9.84097 32.4892 23.021 20.4285 39.0347 20.4285H112.989C129.002 20.4285 142.183 32.501 144.146 48.0651H145.287C148.99 48.0769 152 51.1069 152 54.847Z" fill="white"/><path d="M60.5311 63.0573C61.4667 59.0686 59.0131 55.0716 55.0509 54.1298C51.0886 53.1879 47.1181 55.6579 46.1825 59.6466C45.2469 63.6352 47.7005 67.6322 51.6627 68.5741C55.625 69.5159 59.5955 67.046 60.5311 63.0573Z" fill="white"/><path d="M101.551 68.1996C105.313 66.6311 107.099 62.2899 105.541 58.5033C103.983 54.7167 99.6704 52.9185 95.9088 54.487C92.1473 56.0555 90.361 60.3967 91.9191 64.1834C93.4773 67.97 97.7897 69.7681 101.551 68.1996Z" fill="white"/><path d="M75.7882 15.6943C80.0934 15.6943 83.5834 12.181 83.5834 7.84713C83.5834 3.51328 80.0934 0 75.7882 0C71.4831 0 67.993 3.51328 67.993 7.84713C67.993 12.181 71.4831 15.6943 75.7882 15.6943Z" fill="white"/><path d="M77.6342 13.5638H73.9541V23.5414H77.6342V13.5638Z" fill="white"/></svg>';
+  r = document.createElement("div");
+  (r.style.position = "fixed"),
+    (r.style.bottom = "20px"),
+    (r.style.right = "20px"),
+    (r.style.width = "60px"),
+    (r.style.height = "60px"),
+    (r.style.backgroundColor = e),
+    (r.style.borderRadius = "50%"),
+    (r.style.color = "#fff"),
+    (r.style.display = "flex"),
+    (r.style.justifyContent = "center"),
+    (r.style.alignItems = "center"),
+    (r.style.cursor = "pointer"),
+    (r.style.zIndex = "99999"),
+    (r.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px"),
+    (r.innerHTML = o),
+    r.setAttribute("tabindex", "-1");
 
-    // Id bot
-    let id = await t;
-
-    const apiUrl = `https://owlee-dev.thinklabs.com.vn/chatbot-iframe/${id}`;
-    async function fetchData() {
-        try {
-            const response = await fetch(apiUrl);
-
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-            const data = await response.json();
-            return data.isPublic;
-        } catch (error) {
-            console.error("error:", error);
-        }
-    }
-
-    async function CheckPublicBot() {
-        const check = await fetchData();
-
-        if (check === true) {
-            let i = `https://gptsalebot.com/chat-app/${id ? id : null}`,
-                r = document.createElement("div");
-            r.className = "box-chat-embed";
-            r.style.position = "fixed";
-            r.style.bottom = "20px";
-            r.style.right = "20px";
-            r.style.borderRadius = "50%";
-            r.style.color = "#fff";
-            r.style.display = "flex";
-            r.style.justifyContent = "center";
-            r.style.alignItems = "center";
-            r.style.cursor = "pointer";
-            r.style.zIndex = "99999";
-
-            const imageUrl = document.createElement("img");
-            //css imageUrl
-
-            imageUrl.src = `${
-                iconLogo
-                    ? iconLogo
-                    : "https://mindmaid.ai/banner/avatar/Bot-taÌch-neÌ‚Ì€n-.gif"
-            }`;
-
-            imageUrl.style.width = "65px";
-            imageUrl.style.backgroundColor = "#3075FF";
-            imageUrl.style.borderRadius = "50%";
-
-            // imageUrl.style.height = "fill";
-
-            const greetingBox = document.createElement("div");
-
-            greetingBox.style.background = e;
-            greetingBox.className = "greetingBox";
-            greetingBox.style.borderRadius = "10px 10px 10px 0px";
-            greetingBox.style.maxWidth = "140px";
-            greetingBox.style.position = "relative";
-            greetingBox.style.padding = "6px 10px";
-
-            const greetingText = document.createElement("p");
-
-            greetingText.className = "greetingText";
-            // greetingText.textContent = "Chat ngay";
-            greetingText.style.color = "white";
-            greetingText.style.fontSize = "15px";
-            greetingText.style.fontWeight = "600";
-            greetingText.style.marginBlockEnd = "0";
-            greetingText.style.marginBlockStart = "0";
-            greetingText.style.whiteSpace = "nowrap";
-            greetingText.style.fontStyle = "italic";
-            greetingText.style.marginTop = "3px";
-
-            //css icon box
-            const iconBox = document.createElement("div");
-            iconBox.innerHTML = `<svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path id="Vector26" d="M0 22V0.5L14 8L0 22Z" fill="${e}" /></svg>`;
-
-            const boxIcon = document.createElement("div");
-            boxIcon.style.display = "flex";
-
-            //css iconBox
-            iconBox.style.position = "absolute";
-            iconBox.style.left = "0";
-            iconBox.style.bottom = "-13px";
-
-            const iconHand = document.createElement("img");
-            // css iconHand
-            iconHand.src =
-                "https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif";
-            iconHand.style.width = "20px";
-            iconHand.style.height = "20px";
-            iconHand.style.marginLeft = "10px";
-
-            boxIcon.appendChild(greetingText);
-            boxIcon.appendChild(iconHand);
-
-            r.appendChild(imageUrl);
-
-            r.style.animation = "slideInFromRight 1s ease";
-            r.setAttribute("tabindex", "-1");
-
-            let $ = document.createElement("iframe");
-            ($.style.zIndex = "999"),
-                ($.style.position = "fixed"),
-                ($.style.bottom = "30px"),
-                ($.style.right = "100px"),
-                ($.style.width = l),
-                ($.style.height = s),
-                ($.style.border = "none"),
-                ($.style.display = "none"),
-                ($.style.borderRadius = "16.43px"),
-                ($.style.boxShadow =
-                    "0px 6.765199184417725px 12.56394100189209px -2.8993711471557617px #00000040"),
-                ($.style.opacity = "0"),
-                ($.style.transition = "opacity 0.2s ease-in-out"),
-                ($.style.userSelect = "none"),
-                ($.style.maxHeight = "73vh"),
-                ($.style.maxWidth = "90vw"),
-                $.style.setProperty("-moz-user-select", "none"),
-                $.style.setProperty("-webkit-user-select", "none"),
-                $.style.setProperty("-khtml-user-select", "none"),
-                ($.src = i);
-
-            let a = !1;
-            r.addEventListener("click", () => {
-                a
-                    ? (($.style.opacity = "0"),
-                        setTimeout(() => {
-                            $.style.display = "none";
-                        }, 10),
-                        (r.style.backgroundColor = "revert"),
-                        (r.style.boxShadow = "revert"),
-                        (r.style.width = "revert"),
-                        (r.style.height = "revert"),
-                        (a = !1),
-                        (r.innerHTML = ""),
-                        r.appendChild(imageUrl))
-                    : (($.style.display = "block"),
-                        setTimeout(() => {
-                            $.style.opacity = "1";
-                            r.style.backgroundColor = e;
-                            r.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
-                            r.style.width = "60px";
-                            r.style.height = "60px";
-                        }, 0),
-                        (a = !0),
-                        (r.innerHTML =
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg>'));
-            }),
-                document.body.appendChild(r),
-                document.body.appendChild($);
-        } else {
-            return;
-        }
-    }
-
-    CheckPublicBot();
+  let $ = document.createElement("iframe");
+  ($.style.zIndex = "999"),
+    ($.style.position = "fixed"),
+    ($.style.bottom = "100px"),
+    ($.style.right = "20px"),
+    ($.style.width = l),
+    ($.style.height = s),
+    ($.style.border = "1px solid #ffffff00"),
+    ($.style.display = "none"),
+    ($.style.borderRadius = "10px"),
+    ($.style.boxShadow =
+      "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"),
+    ($.style.opacity = "0"),
+    ($.style.transition = "opacity 0.2s ease-in-out"),
+    ($.style.userSelect = "none"),
+    ($.style.maxHeight = "73vh"),
+    ($.style.maxWidth = "90vw"),
+    $.style.setProperty("-moz-user-select", "none"),
+    $.style.setProperty("-webkit-user-select", "none"),
+    $.style.setProperty("-khtml-user-select", "none"),
+    ($.src = i);
+  let a = !1;
+  r.addEventListener("click", () => {
+    a
+      ? (($.style.opacity = "0"),
+        setTimeout(() => {
+          $.style.display = "none";
+        }, 200),
+        (a = !1),
+        (r.innerHTML = o))
+      : (($.style.display = "block"),
+        setTimeout(() => {
+          $.style.opacity = "1";
+        }, 0),
+        (a = !0),
+        (r.innerHTML =
+          '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg>'));
+  }),
+    document.body.appendChild(r),
+    document.body.appendChild($);
 }
-
 function ready(t) {
-    if ("loading" !== document.readyState) {
-        t();
-        return;
-    }
-    document.addEventListener("DOMContentLoaded", t);
+  if ("loading" !== document.readyState) {
+    t();
+    return;
+  }
+  document.addEventListener("DOMContentLoaded", t);
 }
-
 !(function () {
-    let t = document
-            .querySelector('script[data-chat-service="Salebot"][data-bot-id]')
-            .getAttribute("data-bot-id"),
-        e =
-            document
-                .querySelector('script[data-chat-service="Salebot"][data-bubble-color]')
-                ?.getAttribute("data-bubble-color") ?? "rgb(57 186 248)",
-        l =
-            document
-                .querySelector('script[data-chat-service="Salebot"][data-chat-width]')
-                ?.getAttribute("data-chat-width") ?? "450px",
-        iconLogo = document
-            .querySelector('script[data-chat-service="Salebot"][data-icon-logo]')
-            ?.getAttribute("data-icon-logo"),
-        s =
-            document
-                .querySelector('script[data-chat-service="Salebot"][data-chat-height]')
-                ?.getAttribute("data-chat-height") ?? "550px";
-    ready(() => init(t, e, l, s, iconLogo));
+  let t = document
+      .querySelector('script[data-chat-service="Salebot"][data-bot-id]')
+      .getAttribute("data-bot-id"),
+    e =
+      document
+        .querySelector('script[data-chat-service="Salebot"][data-bubble-color]')
+        .getAttribute("data-bubble-color"),
+    l =
+      document
+        .querySelector('script[data-chat-service="Salebot"][data-chat-width]')
+        .getAttribute("data-chat-width"),
+    s =
+      document
+        .querySelector('script[data-chat-service="Salebot"][data-chat-height]')
+        .getAttribute("data-chat-height");
+  ready(() => init(t, e, l, s));
 })();
